d60a467088098e5a9ed36e1dca1835dbef7c0e3e
Qasem Zalmai <mohammadqaseem.zalmai@smartnews.com>
2024-04-11 16:53:07 -0700

    new group added

--- Okta-Group-Operations/group.tf
+++ Okta-Group-Operations/group.tf
@@ -8,3 +8,7 @@ resource "okta_group" "Terraform-TEST1" {
     name        = "Terraform-TEST1"
     description = "Terrafrom TEST1 Group Description"
 }
+
+resource "okta_group" "AAA_NewGroup" {
+  name = "AAA_NewGroup"
+}
