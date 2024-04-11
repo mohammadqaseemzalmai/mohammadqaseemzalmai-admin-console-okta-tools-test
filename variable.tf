3e2f940dd03bd0c8ffa8e598f54df2c8aa8eb22e
Qasem Zalmai <mohammadqaseem.zalmai@smartnews.com>
2024-04-11 16:38:50 -0700

    updated yml

--- variable.tf
+++ variable.tf
@@ -1,19 +1,19 @@
-# variable "private_key" {
+variable "private_key" {
+  type = string
+}
+
+variable "client_id" {
+  type = string
+}
+
+variable "private_key_id" {
+  type = string
+}
+
+variable "org_name" {
+  type = string
+}
+
+# variable "base_url" {
 #   type = string
 # }
-
-# variable "client_id" {
-#   type = string
-# }
-
-# variable "private_key_id" {
-#   type = string
-# }
-
-# variable "org_name" {
-#   type = string
-# }
-
-# # variable "base_url" {
-# #   type = string
-# # }
